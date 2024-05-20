00680279fd1ad041089b39c82a74e34832704567
Farid Mammadov <faridmmmdv@gmail.com>
2024-05-20 20:02:43 +0400

    stable optimized version of api

--- app/gamePageParser.js
+++ app/gamePageParser.js
@@ -7,108 +7,92 @@ async function fetchGameScore(game) {
 
     const browser = await puppeteer.launch({  
         headless: true,
-        defaultViewport: null,
-        args: ['--no-sandbox', '--disable-features=site-per-process'],
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--disable-gpu',
+            '--window-size=1280x800',
+            '--disable-features=site-per-process'
+        ],
+        defaultViewport: {
+            width: 1280,
+            height: 800
+        },
         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     });
 
     const page = await browser.newPage();
+    
+    // Intercept requests to block unnecessary resources
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+        const resourceType = request.resourceType();
+        if (['image', 'stylesheet', 'font'].includes(resourceType)) {
+            request.abort();
+        } else {
+            request.continue();
+        }
+    });
+
     await page.goto(game, { waitUntil: 'networkidle2' });
 
     try {
         await page.waitForSelector('.smv__incidentsHeader.section__title');
-        // console.log('Selector found', page.url());
 
-        const scores = await page.evaluate(() => 
-            Array.from(document.querySelectorAll('.smv__incidentsHeader.section__title'), element => element.innerHTML)
-        );
+        const gameData = await page.evaluate(() => {
+            const scores = Array.from(document.querySelectorAll('.smv__incidentsHeader.section__title'), element => element.innerHTML);
+            const firstHalf = scores[0].split('</div><div>')[1].replace('</div>', '').trim();
+            const secondHalf = scores.length > 1 ? scores[1].split('</div><div>')[1].replace('</div>', '').trim() : null;
+            const title = document.title;
 
-        // console.log(scores[0]); // Debugging log
-        // console.log(scores[1]); // Debugging log
+            const getStats = (className, team) => {
+                return Array.from(document.getElementsByClassName(className)).map(element => ({
+                    'type': element.querySelector('title').innerHTML,
+                    'time': parseInt(element.innerText.split('\n')[0]),
+                    'info': element.innerText.split('\n')[1] ?? 'No info',
+                    'team': team,
+                }));
+            };
 
-        const firstHalf = scores[0].split('</div><div>')[1].replace('</div>', '').trim();
-        var secondHalf = null;
+            const homeStats = getStats('smv__homeParticipant', 'home');
+            const awayStats = getStats('smv__awayParticipant', 'away');
+            const timeline = homeStats.concat(awayStats).sort((a, b) => a.time - b.time);
 
-        if (scores.length > 1){
-            secondHalf = scores[1].split('</div><div>')[1].replace('</div>', '').trim();
-        }
+            const totalScore = document.querySelector('.detailScore__wrapper').innerText.replaceAll('\n', ' ');
 
-        const title = await page.evaluate(() => 
-            document.title
-        );
-
-        const awayStats = await page.evaluate(() => {
-            const stats = [];
-
-            const elements = document.getElementsByClassName('smv__awayParticipant');
-            for (let i = 0; i < elements.length; i++) {
-                stats.push({
-                    'type': elements[i].querySelector('title').innerHTML,
-                    'time': parseInt(elements[i].innerText.split('\n')[0]),
-                    'info': elements[i].innerText.split('\n')[1] ?? 'No info',
-                    'team': 'away',
-                });
-            }
-
-            return stats;
-        });
-
-        const homeStats = await page.evaluate(() => {
-            const stats = [];
-
-            const elements = document.getElementsByClassName('smv__homeParticipant');
-            for (let i = 0; i < elements.length; i++) {
-                stats.push({
-                    'type': elements[i].querySelector('title').innerHTML,
-                    'time': parseInt(elements[i].innerText.split('\n')[0]),
-                    'info': elements[i].innerText.split('\n')[1] ?? 'No info',
-                    'team': 'home',
-                });
-            }
-
-            return stats;
-        });
-
-        const timeline = homeStats.concat(awayStats).sort((a, b) => a.time - b.time);
-
-        const totalScore = await page.evaluate(() => {
-            return document.querySelector('.detailScore__wrapper').innerText.replaceAll('\n', ' ');
-        });
-
-        // name format is ШАР 1-0 ЭЙП | Шарлеруа - Эйпен | Обзор
-
-        const name = title.split('|')[1].trim();
-
-        const details = await page.evaluate(() => {
-            const startedAt = document.querySelector('.duelParticipant__startTime').innerText;
-            const status = document.querySelector('.fixedHeaderDuel__detailStatus').innerText;
-            const timestamp = document.querySelector('.eventAndAddedTime').innerText;
+            const details = {
+                startedAt: document.querySelector('.duelParticipant__startTime').innerText,
+                status: document.querySelector('.fixedHeaderDuel__detailStatus').innerText,
+                timestamp: document.querySelector('.eventAndAddedTime').innerText,
+            };
 
             return {
-                startedAt,
-                status,
-                timestamp,
+                firstHalf,
+                secondHalf,
+                title,
+                totalScore,
+                timeline,
+                details,
             };
         });
-
-        // console.log('Halftime score:', firstHalf);
-        // console.log('Score:', secondHalf);
 
         const endTime = Date.now();
 
         return {
-            name,
-            details,
-            totalScore,
-            firstHalf,
-            secondHalf,
-            timeline,
-            'initialBets': {
-                'home': '@todo',
-                'draw': '@todo',
-                'away': '@todo',
+            name: gameData.title.split('|')[1].trim(),
+            details: gameData.details,
+            totalScore: gameData.totalScore,
+            firstHalf: gameData.firstHalf,
+            secondHalf: gameData.secondHalf,
+            timeline: gameData.timeline,
+            initialBets: {
+                home: '@todo',
+                draw: '@todo',
+                away: '@todo',
             },
-            'responseTime': (endTime - startTime) / 1000,
+            responseTime: (endTime - startTime) / 1000,
         };
     } catch (error) {
         console.error('Error fetching game data:', error);
@@ -118,6 +102,5 @@ async function fetchGameScore(game) {
 
     return null;  // Ensure a value is always returned
 }
-
 
 module.exports = fetchGameScore;
