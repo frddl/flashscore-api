e350dc0bd223c7df790ebd51527654012082760a
Farid Mammadov <faridmmmdv@gmail.com>
2024-09-09 00:22:42 +0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,9 @@ module.exports = {
     script : "npm",
     args   : "run start",
     watch  : true, // optional: restarts the app if files change
+    exec_mode: "cluster", // Use PM2's cluster mode for multi-core support
+    instances: "max", // Automatically run as many instances as there are CPU cores
+    max_memory_restart: "512M", // Restart the app if it exceeds 512M memory
   }]
 };
 
